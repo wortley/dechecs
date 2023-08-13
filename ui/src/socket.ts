6c7f1522b9232a7c00831cf3ef497807d9429196
orw22 <oliver@thewortleys.plus.com>
2023-08-13 22:09:11 +0100

    Add API url

--- ui/src/socket.ts
+++ ui/src/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8000";
+  process.env.NODE_ENV === "production"
+    ? "https://wchessapi-1-f5231112.deta.app"
+    : "http://localhost:8000";
 
 export const socket = io(URL ?? "", {
   transports: ["websocket"],
