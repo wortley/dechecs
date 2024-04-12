c0d0fdd0eda40eee17b4e7ed2ca9729467b10e8b
orw22 <oliver@thewortleys.plus.com>
2024-04-12 19:36:38 +0100

    Update unichess header

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -3,7 +3,9 @@ import styles from "./header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h2>unichess</h2>
+      <h2>
+        <i>unichess</i>
+      </h2>
     </header>
   );
 }
