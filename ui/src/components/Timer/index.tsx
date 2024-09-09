3dd2752a6d589c13dee16274194b100191da9558
orw22 <oliver@thewortleys.plus.com>
2024-09-09 17:10:13 +0100

    Improve timer acc

--- ui/src/components/Timer/index.tsx
+++ ui/src/components/Timer/index.tsx
@@ -38,32 +38,40 @@ export default function Timer({ side, timeControl, outcome }: Readonly<TimerProp
 
   useEffect(() => {
     if (outcome && outcome > 0) {
-      clearInterval(timerIntervalId.current)
-      return
+      clearInterval(timerIntervalId.current);
+      return;
     }
-
+  
+    let lastUpdateTime = Date.now();
+  
     const intervalId = setInterval(() => {
       setTimer((prevTimer) => {
+        const now = Date.now();
+        const elapsed = now - lastUpdateTime; // compute time elapsed since last tick
+        lastUpdateTime = now; // update last tick time
+  
         const newTimer = {
-          white: turn === Colour.WHITE ? prevTimer.white - 1000 : prevTimer.white,
-          black: turn === Colour.BLACK ? prevTimer.black - 1000 : prevTimer.black,
-        }
+          white: turn === Colour.WHITE ? prevTimer.white - elapsed : prevTimer.white,
+          black: turn === Colour.BLACK ? prevTimer.black - elapsed : prevTimer.black,
+        };
+  
+        // emit 'flag' event if time runs out for current player
         if (side === Colour.WHITE && turn === Colour.WHITE && newTimer.white <= 0) {
-          socket.emit("flag", Colour.WHITE)
+          socket.emit("flag", Colour.WHITE);
         } else if (side === Colour.BLACK && turn === Colour.BLACK && newTimer.black <= 0) {
-          socket.emit("flag", Colour.BLACK)
+          socket.emit("flag", Colour.BLACK);
         }
-
-        return newTimer
-      })
-    }, 1000)
-
-    timerIntervalId.current = intervalId
-
+  
+        return newTimer;
+      });
+    }, 50);
+  
+    timerIntervalId.current = intervalId;
+  
     return () => {
-      clearInterval(intervalId)
-    }
-  }, [turn, outcome, side])
+      clearInterval(intervalId);
+    };
+  }, [turn, outcome, side]);
 
   return side === Colour.WHITE ? (
     <div className={styles.timer}>
