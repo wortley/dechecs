4b1ff32b7a6da184f31862a61000dbd5000fa41a
orw22 <oliver@thewortleys.plus.com>
2024-09-12 21:50:54 +0100

    header update

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -3,10 +3,13 @@ import styles from "./header.module.css"
 export default function Header() {
   return (
     <header className={styles.header}>
+      <button className={styles.headerLogo} onClick={() => (window.location.href = "/")}>
+        d&eacute;checs
+      </button>
+      <div className={styles.headerPieces}> &#9820; &#9822; &#9821; &#9818; &#9819; &#9821; &#9822; &#9820;</div>
       <div className={styles.w3mContainer}>
         <w3m-button />
       </div>
-      <h2 className={styles.headerLogo}>d&eacute;checs</h2>
     </header>
   )
 }
