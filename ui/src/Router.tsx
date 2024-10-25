daa81e7aa6b032c529a9d8b1af7b7a612ee3ef55
orw22 <oliver@thewortleys.plus.com>
2024-10-25 23:59:24 +0100

    Handle other urls

--- ui/src/Router.tsx
+++ ui/src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Create from "./pages/Create"
 import Home from "./pages/Home"
 import Join from "./pages/Join"
@@ -14,6 +14,7 @@ export default function Router() {
         <Route path="/create" element={<Create />} />
         <Route path="/play" element={<Play />} />
         <Route path="/r" element={<Redirect />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   )
